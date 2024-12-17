2e012a8c6f5818ae41e44cbd9ab305d668a79296
Josue Loria <jloriag@hotmail.com>
2024-12-17 12:10:47 -0600

    User login setup

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,21 @@
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\SellController;
 use App\Http\Controllers\Api\EcommerceDataController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::apiResource('products',ProductController::class);
 
 Route::apiResource('sells',SellController::class);
 
 Route::apiResource('ecommercedata',EcommerceDataController::class);
+
+Route::apiResource('user',UserController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
