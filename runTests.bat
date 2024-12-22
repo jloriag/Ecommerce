4b7650934b629308c8c6aa93532e9b1c2b520fa4
Josue Loria <jloriag@hotmail.com>
2024-12-22 15:59:14 -0600

    test

--- runTests.bat
+++ runTests.bat
@@ -1,5 +1,2 @@
 cd C:\xampp\htdocs\Ecommerce\
 C:\xampp\htdocs\Ecommerce\vendor\bin\phpunit tests
-@echo off
-echo Este es un ejemplo de espera personalizada.
-choice /m "Presione una tecla para salir"
